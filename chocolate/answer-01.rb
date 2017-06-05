9799f426acdb840bdc8c25261bf7bd16208b9dfa
Koichi Sato <ksato@pepabo.com>
2017-06-05 09:13:19 +0900

    一般化した処理に変更

--- chocolate/answer-01.rb
+++ chocolate/answer-01.rb
@@ -1,3 +1,8 @@
-str = 'パタトクカシーー'
-answer = str.split('').values_at(0, 2, 4, 6).join
-puts answer
+input = gets.chomp
+answer = ''
+
+input.split('').each_with_index do |char, i|
+  answer << char if i.even?
+end
+
+p answer
